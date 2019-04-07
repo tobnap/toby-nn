7062400a8d926a4aeff14e6a0ccb7413a32de441
Toby <jakeypaulyguyy@gmail.com>
2019-04-07 05:00:10 -0400

    fixed grid

--- static/js/repo.js
+++ static/js/repo.js
@@ -288,7 +288,7 @@
 
         // Setup repo container
         return this.each(function(){
-            _this.container = $('<div class="repo"><h1><a href="#" data-id="' + _this.namespace + '_split_default">' + _this.settings.name + '</a></h1><div class="loader"></div><div class="page" id="' + _this.namespace + '_split_default"><ul><li class="titles"><span>name</span></li></ul></div></div>').appendTo($(this));
+            _this.container = $('<div class="repo"><h1><a href="#" data-id="' + _this.namespace + '_split_default">' + _this.settings.name + '</a></h1><div class="loader"></div><div class="page" id="' + _this.namespace + '_split_default"><ul><li class="titles"><span>name</span></li></ul></div></div>').appendTo(document.getElementById('textareawrapper'));
         });
     };
 
