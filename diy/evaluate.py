9503f8e63a887745334f8ef03e7dc5619a01ebed
Toby <jakeypaulyguyy@gmail.com>
2019-05-20 18:05:56 -0400

    hopefully fixed MySQL Connection not available error

--- diy/evaluate.py
+++ diy/evaluate.py
@@ -1,12 +1,12 @@
 import web
 import mysql.connector
-from cleardb import mydb
 
 render = web.template.render('html/')
 
 class evaluate:
     def GET(self):
         from web import form
+        from cleardb import mydb
         mycursor = mydb.cursor()
         mycursor.execute("SELECT `index`, name1, name2 FROM weights")
         names = mycursor.fetchall()
